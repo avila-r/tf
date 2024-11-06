7fd262dfc18a2c597d81b26f926711fc3169fc9b
r. ávila <avila.dev@outlook.com>
2024-11-05 23:40:48 -0300

    build: create instances based on available subnets' quantity

--- modules/instances/main.tf
+++ modules/instances/main.tf
@@ -1,5 +1,6 @@
 # EC2 instance with a public IP in the public subnet
 resource "aws_instance" "avila-cli-public-instance" {
+  count         = length(var.public_subnets)
   ami           = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
 
@@ -7,16 +8,17 @@ resource "aws_instance" "avila-cli-public-instance" {
     aws_security_group.avila-cli-public-instance-sg.id
   ]
 
-  subnet_id                   = var.public_subnets[0].id
+  subnet_id                   = var.public_subnets[count.index].id
   associate_public_ip_address = true # Assign a public IP
 
   tags = {
-    Name = "avila-cli-public-instance"
+    Name = "avila-cli-public-instance-0${count.index + 1}"
   }
 }
 
 # EC2 instance without a public IP in the private subnet
 resource "aws_instance" "avila-cli-private-instance" {
+  count         = length(var.private_subnets)
   ami           = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
 
@@ -24,10 +26,10 @@ resource "aws_instance" "avila-cli-private-instance" {
     aws_security_group.avila-cli-private-instance-sg.id
   ]
 
-  subnet_id                   = var.private_subnets[0].id
+  subnet_id                   = var.private_subnets[count.index].id
   associate_public_ip_address = false # No public IP assigned
 
   tags = {
-    Name = "avila-cli-private-instance"
+    Name = "avila-cli-private-instance-0${count.index + 1}"
   }
 }
