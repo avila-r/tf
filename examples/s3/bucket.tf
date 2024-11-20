630989a8e2da155b41f83dfa4c4f47242aeb7147
r. ávila <avila.dev@outlook.com>
2024-11-19 21:47:33 -0300

    feat: add s3

--- examples/s3/bucket.tf
+++ examples/s3/bucket.tf
@@ -1,1 +1,14 @@
-resource "aws_s3_bucket" "example" {}
+resource "aws_s3_bucket" "name" {
+  bucket = "dksahjdoiashdnoa"
+
+  tags = {
+    Name = "Example S3 Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "config" {
+  bucket = aws_s3_bucket.name.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
